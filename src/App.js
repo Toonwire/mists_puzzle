d3f30bf3035113950df231e34ab74dd7259bf777
Toonwire <Toonwire@hotmail.com>
2020-10-10 13:08:58 +0200

    init setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,142 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+import MistIcon from './MistIcon';
+import IconButton from '@material-ui/core/IconButton';
+import Grid from '@material-ui/core/Grid';
+
+const possible = [
+  {
+    shape: "flower",
+    filled: true,
+    ring: true,
+  },
+  {
+    shape: "flower",
+    filled: true,
+    ring: false,
+  },
+  {
+    shape: "flower",
+    filled: false,
+    ring: true,
+  },
+  {
+    shape: "flower",
+    filled: false,
+    ring: false,
+  },
+  {
+    shape: "leaf",
+    filled: true,
+    ring: true,
+  },
+  {
+    shape: "leaf",
+    filled: true,
+    ring: false,
+  },
+  {
+    shape: "leaf",
+    filled: false,
+    ring: true,
+  },
+  {
+    shape: "leaf",
+    filled: false,
+    ring: false,
+  },
+];
+
+const generateRandoms = () => {
+
+    let randoms = [];
+    let correctIndex = -1;
+
+    while (correctIndex === -1) {
+      let numFlowers = 0, numLeaves = 0;
+      let numFilled = 0, numOutlined = 0; 
+      let numRings = 0, numNoRings = 0;
+
+      randoms = [];
+      for (let i = 0; i < 4; i++) {
+        const random = possible[Math.floor(Math.random() * possible.length)];
+        randoms.push(random);
+
+        if (random.shape === "flower") numFlowers++;
+        else if (random.shape === "leaf") numLeaves++;
+        
+        if (random.filled) numFilled++;
+        else numOutlined++;
+
+        if (random.ring) numRings++;
+        else numNoRings++;
+      }
+
+      if (numFlowers === 1 && numLeaves !== 1 
+        && numFilled !== 1 && numOutlined !== 1
+        && numRings !== 1 && numNoRings !== 1) 
+        correctIndex = randoms.findIndex(random => random.shape === "flower");
+
+      else if (numFlowers !== 1 && numLeaves === 1 
+        && numFilled !== 1 && numOutlined !== 1
+        && numRings !== 1 && numNoRings !== 1) 
+        correctIndex = randoms.findIndex(random => random.shape === "leaf");
+
+      else if (numFlowers !== 1 && numLeaves !== 1 
+        && numFilled === 1 && numOutlined !== 1
+        && numRings !== 1 && numNoRings !== 1) 
+        correctIndex = randoms.findIndex(random => random.filled);
+
+      else if (numFlowers !== 1 && numLeaves !== 1 
+        && numFilled !== 1 && numOutlined === 1
+        && numRings !== 1 && numNoRings !== 1) 
+        correctIndex = randoms.findIndex(random => !random.filled);
+
+      else if (numFlowers !== 1 && numLeaves !== 1 
+        && numFilled !== 1 && numOutlined !== 1
+        && numRings === 1 && numNoRings !== 1) 
+        correctIndex = randoms.findIndex(random => random.ring);
+
+      else if (numFlowers !== 1 && numLeaves !== 1 
+        && numFilled !== 1 && numOutlined !== 1
+        && numRings !== 1 && numNoRings === 1) 
+        correctIndex = randoms.findIndex(random => !random.ring);
+    }
+
+    return [randoms, correctIndex]; 
+}
 
 function App() {
+  const [randoms, setRandoms] = useState([]);
+  const [score, setScore] = useState(0);
+  const [correctIndex, setCorrectIndex] = useState(-1);
+
+  useEffect(() => {
+    const [newRandoms, newCorrectIndex] = generateRandoms();
+    setRandoms(newRandoms);
+    setCorrectIndex(newCorrectIndex);
+  }, [score])
+
+  const handleClick = (index) => {
+    if (index === correctIndex) setScore(score + 1);
+    else {
+      alert("BAD");
+      setScore(0);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    {`Score: ${score}`} 
+    <Grid container spacing={3}>
+      {randoms.map((random, index) => (
+        <Grid key={index} item xs={3}>
+          <IconButton onClick={() => handleClick(index)}>
+            <MistIcon shape={random.shape} filled={random.filled} ring={random.ring} />
+          </IconButton>
+        </Grid>
+      ))}
+    </Grid>
     </div>
   );
 }
