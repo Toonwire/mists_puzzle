d6e51a5afcf465839a135cb597370f2d20675555
Lukas Villumsen <toonwire@hotmail.com>
2020-11-27 10:37:11 +0100

    use direct typography import

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import IconButton from '@material-ui/core/IconButton';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Alert from '@material-ui/lab/Alert';
-import { Typography } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
 
 const combinations = [
   {
