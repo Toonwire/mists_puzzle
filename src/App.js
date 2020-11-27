ca54438042790fe6ae577f088fed191004fd13bc
Lukas Villumsen <toonwire@hotmail.com>
2020-11-27 10:36:29 +0100

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React, { useState, useEffect } from 'react';
 
-import {makeStyles} from '@material-ui/core/styles';
 import MistIcon from './MistIcon';
 import IconButton from '@material-ui/core/IconButton';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
+import Alert from '@material-ui/lab/Alert';
+import { Typography } from '@material-ui/core';
 
-const useStyles = makeStyles((theme) => ({
-  visible: {
-  	visibility: "visible",
-  },
-  hidden: {
-  	visibility: "hidden",
-  }
-}));
-
-const possible = [
+const combinations = [
   {
     shape: "flower",
     filled: true,
@@ -60,113 +52,135 @@ const possible = [
 
 const generateRandoms = () => {
 
-    let randoms = [];
-    let correctIndex = -1;
+  let randoms = [];
+  let correctIndex = -1;
 
-    while (correctIndex === -1) {
-      let numFlowers = 0, numLeaves = 0;
-      let numFilled = 0, numOutlined = 0; 
-      let numRings = 0, numNoRings = 0;
+  while (correctIndex === -1) {
+    let numFlowers = 0, numLeaves = 0;
+    let numFilled = 0, numOutlined = 0;
+    let numRings = 0, numNoRings = 0;
 
-      randoms = [];
-      for (let i = 0; i < 4; i++) {
-        const random = possible[Math.floor(Math.random() * possible.length)];
-        randoms.push(random);
+    randoms = [];
+    for (let i = 0; i < 4; i++) {
+      const random = combinations[Math.floor(Math.random() * combinations.length)];
+      randoms.push(random);
 
-        if (random.shape === "flower") numFlowers++;
-        else if (random.shape === "leaf") numLeaves++;
-        
-        if (random.filled) numFilled++;
-        else numOutlined++;
+      if (random.shape === "flower") numFlowers++;
+      else if (random.shape === "leaf") numLeaves++;
 
-        if (random.ring) numRings++;
-        else numNoRings++;
-      }
+      if (random.filled) numFilled++;
+      else numOutlined++;
 
-      if (numFlowers === 1 && numLeaves !== 1 
-        && numFilled !== 1 && numOutlined !== 1
-        && numRings !== 1 && numNoRings !== 1) 
-        correctIndex = randoms.findIndex(random => random.shape === "flower");
-
-      else if (numFlowers !== 1 && numLeaves === 1 
-        && numFilled !== 1 && numOutlined !== 1
-        && numRings !== 1 && numNoRings !== 1) 
-        correctIndex = randoms.findIndex(random => random.shape === "leaf");
-
-      else if (numFlowers !== 1 && numLeaves !== 1 
-        && numFilled === 1 && numOutlined !== 1
-        && numRings !== 1 && numNoRings !== 1) 
-        correctIndex = randoms.findIndex(random => random.filled);
-
-      else if (numFlowers !== 1 && numLeaves !== 1 
-        && numFilled !== 1 && numOutlined === 1
-        && numRings !== 1 && numNoRings !== 1) 
-        correctIndex = randoms.findIndex(random => !random.filled);
-
-      else if (numFlowers !== 1 && numLeaves !== 1 
-        && numFilled !== 1 && numOutlined !== 1
-        && numRings === 1 && numNoRings !== 1) 
-        correctIndex = randoms.findIndex(random => random.ring);
-
-      else if (numFlowers !== 1 && numLeaves !== 1 
-        && numFilled !== 1 && numOutlined !== 1
-        && numRings !== 1 && numNoRings === 1) 
-        correctIndex = randoms.findIndex(random => !random.ring);
+      if (random.ring) numRings++;
+      else numNoRings++;
     }
 
-    return [randoms, correctIndex]; 
+    if (numFlowers === 1 && numLeaves !== 1
+      && numFilled !== 1 && numOutlined !== 1
+      && numRings !== 1 && numNoRings !== 1)
+      correctIndex = randoms.findIndex(random => random.shape === "flower");
+
+    else if (numFlowers !== 1 && numLeaves === 1
+      && numFilled !== 1 && numOutlined !== 1
+      && numRings !== 1 && numNoRings !== 1)
+      correctIndex = randoms.findIndex(random => random.shape === "leaf");
+
+    else if (numFlowers !== 1 && numLeaves !== 1
+      && numFilled === 1 && numOutlined !== 1
+      && numRings !== 1 && numNoRings !== 1)
+      correctIndex = randoms.findIndex(random => random.filled);
+
+    else if (numFlowers !== 1 && numLeaves !== 1
+      && numFilled !== 1 && numOutlined === 1
+      && numRings !== 1 && numNoRings !== 1)
+      correctIndex = randoms.findIndex(random => !random.filled);
+
+    else if (numFlowers !== 1 && numLeaves !== 1
+      && numFilled !== 1 && numOutlined !== 1
+      && numRings === 1 && numNoRings !== 1)
+      correctIndex = randoms.findIndex(random => random.ring);
+
+    else if (numFlowers !== 1 && numLeaves !== 1
+      && numFilled !== 1 && numOutlined !== 1
+      && numRings !== 1 && numNoRings === 1)
+      correctIndex = randoms.findIndex(random => !random.ring);
+  }
+
+  return [randoms, correctIndex];
+}
+const Status = {
+  CORRECT: {
+    msg: "Correct guess",
+  },
+  WRONG: {
+    msg: "Wrong guess",
+  },
+  DEFAULT: {
+    msg: "Find the odd one out!",
+  },
 }
 
 function App() {
-  const classes = useStyles();
   const [randoms, setRandoms] = useState([]);
   const [score, setScore] = useState(0);
   const [correctIndex, setCorrectIndex] = useState(-1);
   const [show, setShow] = useState(-1);
+  const [status, setStatus] = useState(Status.DEFAULT);
 
   useEffect(() => {
     const [newRandoms, newCorrectIndex] = generateRandoms();
     setRandoms(newRandoms);
     setCorrectIndex(newCorrectIndex);
-  }, [score])
+  }, [score]);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setStatus(Status.DEFAULT);
+    }, 2000);
+
+    return () => clearTimeout(timeout);
+  }, [status]);
 
   const handleClick = (index) => {
-    if (index === correctIndex) setScore(score + 1);
-    else {
-      alert("BAD");
+    if (index === correctIndex) {
+      setScore(score + 1);
+      setStatus(Status.CORRECT);
+    } else {
       setScore(0);
+      setStatus(Status.WRONG);
     }
   }
-  console.log(show)
-
 
   return (
-    <div>
-      <Box p={2}>
-        {`Score: ${score}`} 
+    <Box display="flex" flexDirection="column" p={4}>
+      <Box display="flex" flexDirection="column" alignItems="center" width={400} m="auto" mb={5}>
+        <Typography variant="h6" gutterBottom>{`Score: ${score}`}</Typography>
+        {status === Status.CORRECT && <Alert severity="success">{Status.CORRECT.msg}</Alert>}
+        {status === Status.WRONG && <Alert severity="error">{Status.WRONG.msg}</Alert>}
+        {status === Status.DEFAULT && <Alert severity="info">{Status.DEFAULT.msg}</Alert>}
       </Box>
-      <Grid container style={{justifyContent: "space-around"}}>
+      <Grid container style={{ justifyContent: "space-around" }}>
         {randoms.map((random, index) => (
-          <Grid item key={index}  style={{textAlign: "center"}} >
-          	{show === index ? (
-          		<IconButton 
-	              onClick={() => handleClick(index)} 
-	              onMouseLeave={() => setShow(-1)}
-            	>
-	              <MistIcon shape={random.shape} filled={random.filled} ring={random.ring} />
-	            </IconButton>
-        	) : (<IconButton 
-	              onMouseEnter={() => setShow(index)}
-            	>
-            	<Box style={{width: 120, height: 120, background: "#bbdefb", borderRadius: "50%"}}></Box>
-	            </IconButton>
-	            )
-	        }
-            	
+          <Grid item key={index} style={{ textAlign: "center" }} >
+            {show === index ? (
+              <IconButton
+                onClick={() => handleClick(index)}
+                onMouseLeave={() => setShow(-1)}
+              >
+                <MistIcon shape={random.shape} filled={random.filled} ring={random.ring} />
+              </IconButton>
+            ) : (
+                <IconButton
+                  onMouseEnter={() => setShow(index)}
+                >
+                  <Box style={{ width: 120, height: 120, background: "#bbdefb", borderRadius: "50%" }}></Box>
+                </IconButton>
+              )
+            }
           </Grid>
         ))}
       </Grid>
-    </div>
+    </Box>
   );
 }
 
